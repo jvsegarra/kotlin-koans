96efd119e288532964a25cc14763d48aab2d6395
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2016-01-31 21:36:02 +0100

    ii_23 solution

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -1,18 +1,17 @@
 package ii_collections
 
-fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    // Return the set of customers who ordered the specified product
-    todoCollectionTask()
-}
+fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> = this.customers.filter {
+    it.orders.filter { it.products.contains(product) }.isNotEmpty()
+}.toSet()
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return orders.filter { it.isDelivered == true }.flatMap { it.products }.maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return this.customers.flatMap { it.orders }.flatMap { it.products }.filter { it == product }.size
 }
