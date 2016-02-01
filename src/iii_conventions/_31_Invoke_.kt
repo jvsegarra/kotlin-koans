8d140340a735ad0bedeab0d58d5ca071667d8072
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2016-02-02 00:04:46 +0100

    iii_31 solution

--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,17 +3,26 @@ package iii_conventions
 import util.*
 
 
-class Invokable
+class Invokable {
+    var numberOfInvocations = 0;
+
+    operator fun invoke(): Invokable {
+        numberOfInvocations++;
+        return this;
+    }
+
+}
+
+public fun Invokable.getNumberOfInvocations() = this.numberOfInvocations
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change class Invokable to count the number of invocations (round brackets).
         Uncomment the commented code - it should return 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
