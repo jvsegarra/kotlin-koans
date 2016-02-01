0dfad33474e8461f33344fd09322d6a42b1bd97f
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2016-02-01 22:19:15 +0100

    iii_25 solution

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,15 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val myCalendar: Calendar = GregorianCalendar(year, month, dayOfMonth)
+        val otherCalendar: Calendar = GregorianCalendar(other.year, other.month, other.dayOfMonth)
+
+        return myCalendar.compareTo(otherCalendar);
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
