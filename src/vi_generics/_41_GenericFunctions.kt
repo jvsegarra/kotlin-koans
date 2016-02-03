a00154674f02450723ebfea957957b6f95f053f5
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2016-02-03 19:13:55 +0100

    vi_41 solution

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -5,7 +5,7 @@ import java.util.HashSet
 import util.TODO
 
 fun task41(): Nothing = TODO(
-    """
+        """
         Task41.
         Add a 'partitionTo' function that splits a collection into two collections according to a predicate.
         Uncomment the commented invocations of 'partitionTo' below and make them compile.
@@ -20,12 +20,15 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C : MutableCollection<in T>> Iterable<T>.partitionTo(posColl: C, negColl: C, predicate: (T) -> Boolean): Pair<C, C> {
+    val (pos, neg) = this.partition(predicate)
+    return Pair(pos.toCollection(posColl), neg.toCollection(negColl))
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
 }
