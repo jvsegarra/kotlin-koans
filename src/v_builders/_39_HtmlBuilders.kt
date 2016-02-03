9dde4af891a0f3ee8c66f840d65a52db53ecd0dd
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2016-02-03 18:52:27 +0100

    v_39 solution

--- src/v_builders/_39_HtmlBuilders.kt
+++ src/v_builders/_39_HtmlBuilders.kt
@@ -5,23 +5,23 @@ import v_builders.htmlLibrary.*
 import util.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(index: Int, row: Int) = if ((index + row) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(index: Int, row: Int) = if ((index + row) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun todoTask39(): Nothing = TODO(
-    """
+        """
         Task 39.
         1) Fill the table with the proper values from products.
         2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
         Pass a color as an argument to functions 'tr', 'td'.
         You can run the 'Html Demo' configuration in IntelliJ to see the rendered table.
     """,
-    documentation = doc39()
+        documentation = doc39()
 )
 
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -33,7 +33,22 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+
+            var row: Int = 0;
+            for (product in products) {
+                tr {
+                    td(color = getCellColor(0, row)) {
+                        text(product.description)
+                    }
+                    td(color = getCellColor(1, row)) {
+                        text(product.price)
+                    }
+                    td(color = getCellColor(2, row)) {
+                        text(product.popularity)
+                    }
+                }
+                row++;
+            }
         }
     }.toString()
 }
